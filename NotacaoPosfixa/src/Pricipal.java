68e8490f941063e8169e72be6af04dbc723c4bc2
gilberto <gilberto@pcpositivo1>
2019-04-01 22:26:33 -0300

    correção de falhas

--- NotacaoPosfixa/src/Pricipal.java
+++ NotacaoPosfixa/src/Pricipal.java
@@ -29,25 +29,48 @@ public class Pricipal {
 			}
 			else if(c == '*' || c == '/' || c == '+' || c =='-'){
 				
-				if(c==(char)pilha.retornatopo()) {
+				
+				 if(pilha.vazio()) {
+					
+					pilha.empilha(c);
+				}
+				 else if(c==(char)pilha.retornatopo() ) {
 					
 					posfixa[i] = c;
+					i++;
+				}
+				
+				else if((char)pilha.retornatopo()=='(') {
+					
+					pilha.empilha(c);
 					
 				}
+				
+				
 				else if((c=='+' || c=='-' ) && ((char)pilha.retornatopo()=='/' || (char)pilha.retornatopo()=='*')){
 					
 					
 					posfixa[i]=(char)pilha.retornatopo();
-					
+					i++;
 					pilha.desempilha();
-					
+					pilha.empilha(c);
 					
 					
 				}
-				else if(!(c=='+' || c=='-' ) && ((char)pilha.retornatopo()=='/' || (char)pilha.retornatopo()=='*')) {
+			
+				
+				
+				else if(!(c=='+' || c=='-' ) && !((char)pilha.retornatopo()=='/' || (char)pilha.retornatopo()=='*')) {
+					
+					
 					
 					
 					posfixa[i] = c;
+					posfixa[i+1] = (char) pilha.retornatopo();
+					
+					pilha.desempilha();
+					i = i+2;
+				
 					
 					
 				}
@@ -55,33 +78,54 @@ public class Pricipal {
 			}
 			else if(c==')'){
 				
-				
-				posfixa[i]=(char)pilha.retornatopo();
-				
+				if((char)pilha.retornatopo()!='(') {
+					posfixa[i]=(char)pilha.retornatopo();
+					i++;
 				pilha.desempilha();
+				}
+				else {
+					pilha.desempilha();
+					if(!pilha.vazio())  {
+					posfixa[i]=(char)pilha.retornatopo();
+					pilha.desempilha();
+					i++;
+					}
+				}
+				
+				
 				
 			}
 			
-			else if(c==')') {
-				
-				
-				
-				
-			}
+	
 			
 			else {
 				
-				posfixa[i] = c;
 				
+				posfixa[i] = c;
+				i++;
 			}
-			
-			System.out.println(posfixa[i]);
+		
 			
 			
 		}
-			
-			
 		
+		while(!pilha.vazio()) {
+			
+			
+			if((char)pilha.retornatopo()!='(') {
+			posfixa[i] = (char)pilha.retornatopo();
+			i++;
+			}
+			
+			pilha.desempilha();
+		}
+		
+		
+	for(int j=0;j<pilha.numerodeposicoes;j++)
+		if(posfixa[j]!=' ')
+			System.out.print(posfixa[j]);
+			
+	}
 		
 		
 		
@@ -90,4 +134,3 @@ public class Pricipal {
 		
 	}
 
-}
